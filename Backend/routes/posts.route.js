f8b9fc87963ecad488610f434985f6b38ed933fc
spandan pati <spandanpati7@gmail.com>
2025-10-11 10:18:06 +0530

    protect all routes from login.js middelwares

--- Backend/routes/posts.route.js
+++ Backend/routes/posts.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const postrouter = express.Router()
+import {islogin} from '../middlewares/islogin.js'
 
 
 // Components :
@@ -12,10 +13,10 @@ import {
 } from '../components/post.logic.js'
 
 
-postrouter.get('/posts',allposts)
-postrouter.get('/posts/:id',postid)
-postrouter.post('/posts',create_post)
-postrouter.patch('/posts/:id',update_post)
-postrouter.delete('/posts/:id',delete_post)
+postrouter.get('/posts', islogin ,allposts)
+postrouter.get('/posts/:id' ,islogin ,postid)
+postrouter.post('/posts', islogin ,create_post)
+postrouter.patch('/posts/:id',islogin,update_post)
+postrouter.delete('/posts/:id',islogin,delete_post)
 
 export default postrouter
